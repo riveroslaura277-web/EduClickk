57fc4e98b2c76fff657880017e5fd339cdd4fcca
yesid noreña <yesidnorenatorres@gmail.com>
2026-05-08 12:23:53 -0500

    mensaje

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduClick.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
