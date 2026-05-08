b399260ea0ecaf3775fe60831841a7d2a2cc619a
danamanrrique <danamanrrique23@gmail.com>
2026-05-08 12:23:59 -0500

    mis cambios

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduClick.Controllers
+{
+    public class RolesController : Controller
+    {
+        public IActionResult fondoRoles()
+        {
+            return View();
+        }
+
+        public IActionResult Docente()
+        {
+            return View();
+        }
+
+        public IActionResult Estudiante()
+        {
+            return View();
+        }
+
+        public IActionResult Acudiente()
+        {
+            return View();
+        }
+
+        public IActionResult Rector()
+        {
+            return View();
+        }
+    }
+}
