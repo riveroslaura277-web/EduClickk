1425fa84452dd7420df1136cfe9dd9e9780da99d
danamanrrique <danamanrrique23@gmail.com>
2026-05-26 12:24:47 -0500

    crud

---
+++ Views/Registro/listar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EduClick.Views.Registro
+{
+    public class listarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
