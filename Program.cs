073816541aac9d270032ce9cd1eda8aa75258201
danamanrrique <danamanrrique23@gmail.com>
2026-05-29 02:49:09 -0500

    Resolviendo conflictos stash

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
 using EduClick.Data;
-using Microsoft.AspNetCore.Identity;
-using EduClick.Models;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Controllers + Views
 builder.Services.AddControllersWithViews();
 
+// DB Context
 builder.Services.AddDbContext<EduClickContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
-// Configuración de Identity con tu modelo Usuarios
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -27,6 +23,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// 🔥 IMPORTANTE (aunque no uses login todavía, déjalo correcto)
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
