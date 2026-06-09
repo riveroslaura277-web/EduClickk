cc9e09e877ab30d3a457e3181136ce5cd6615815
laura <riveroslaura277@gmail.com>
2026-06-09 12:53:19 -0500

    hice cambios para volver a mi vista

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
-using EduClick.Data;
-using EduClick.Models;
+﻿using EduClick.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<EduClickContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+// Configuración de Identity con tu modelo Usuarios
+
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -21,6 +25,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// 🔥 IMPORTANTE (aunque no uses login todavía, déjalo correcto)
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
