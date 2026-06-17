f8168f8c2c587d7b9d803b8789443b8990e83a5e
laura <riveroslaura277@gmail.com>
2026-06-17 16:11:34 -0500

    agregue cambios

--- Controllers/Evidencias.cs
+++ Controllers/Evidencias.cs
@@ -1,29 +1,29 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EduClick.Controladores
+namespace EduClick.Controllers
 {
-    public class Videos : Controller
+    public class Evidencias : Controller
     {
-        // GET: Videos
+        // GET: Evidencias
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Videos/Details/5
+        // GET: Evidencias/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Videos/Create
+        // GET: Evidencias/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Videos/Create
+        // POST: Evidencias/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -38,13 +38,13 @@ namespace EduClick.Controladores
             }
         }
 
-        // GET: Videos/Edit/5
+        // GET: Evidencias/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Videos/Edit/5
+        // POST: Evidencias/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -59,13 +59,13 @@ namespace EduClick.Controladores
             }
         }
 
-        // GET: Videos/Delete/5
+        // GET: Evidencias/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Videos/Delete/5
+        // POST: Evidencias/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,29 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EduClick.Controladores
+namespace EduClick.Controllers
 {
-    public class SobreNosotros : Controller
+    public class HomeController : Controller
     {
-        // GET: SobreNosotros
+        // GET: HomeController
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: SobreNosotros/Details/5
+        // GET: HomeController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: SobreNosotros/Create
+        // GET: HomeController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: SobreNosotros/Create
+        // POST: HomeController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -38,13 +38,13 @@ namespace EduClick.Controladores
             }
         }
 
-        // GET: SobreNosotros/Edit/5
+        // GET: HomeController/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: SobreNosotros/Edit/5
+        // POST: HomeController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -59,13 +59,13 @@ namespace EduClick.Controladores
             }
         }
 
-        // GET: SobreNosotros/Delete/5
+        // GET: HomeController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: SobreNosotros/Delete/5
+        // POST: HomeController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
