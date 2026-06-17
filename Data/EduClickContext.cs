9836f6577ab3ffb572096d53e1baf69bc1785aab
yesid noreña <yesidnorenatorres@gmail.com>
2026-06-17 11:06:59 -0500

    cambios de yesid

--- Data/EduClickContext.cs
+++ Data/EduClickContext.cs
@@ -15,5 +15,9 @@ namespace EduClick.Data
         public DbSet<Docente> Docentes { get; set; }
         public DbSet<Acudiente> Acudientes { get; set; }
         public DbSet<Rector> Rectores { get; set; }
+        public DbSet<Nota> Notas { get; set; }
+        public DbSet<PeriodoViewModel> Periodos { get; set; }
+        public DbSet<CursoViewModel> Cursos { get; set; }
+        public DbSet<MateriaViewModel> Materias { get; set; }
     }
 }
