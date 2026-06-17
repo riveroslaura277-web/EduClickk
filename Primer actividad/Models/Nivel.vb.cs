1d8f623506d4970085022b95ad44c1b40a2e8202
laura <riveroslaura277@gmail.com>
2026-06-16 23:09:59 -0500

    estoy realizando vistas actividades

---
+++ Primer actividad/Models/Nivel.vb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Public Class Nivel
+
+    Public Property NumeroNivel As Integer
+    Public Property Imagen As String
+    Public Property CantidadCorrecta As Integer
+
+End Class
