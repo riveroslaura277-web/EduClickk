54e88490fdbb5c55d6e7868c6e2ce6b4d1d4b461
laura <riveroslaura277@gmail.com>
2026-06-18 09:01:56 -0500

    realice cambios de eliminacion

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EduClick.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
